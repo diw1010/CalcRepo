77128a5a14b5a4254ed80f3709e4c8b28cd349a5
diwsharma <31534746+diw1010@users.noreply.github.com>
2020-08-07 17:04:39 +0530

    commit2

--- Calc.java
+++ Calc.java
@@ -1,3 +1,9 @@
+  class SampleProject{
+  public static int add(int a,int b){
+    return a+b;
+  }
+  public static int sub(int a,int b){
+    return a-b;
   }
   public static int mul(int a,int b){
     return a*b;
